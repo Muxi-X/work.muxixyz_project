94b667f625b0e41b21db0a6e2c822af5ea633b99
Darren2017 <17362990052@163.com>
2018-08-20 10:45:38 +0800

    feat: project_post & delete

--- work_muxixyz_app/api/project.py
+++ work_muxixyz_app/api/project.py
@@ -28,19 +28,64 @@ def project_new(uid):
     try:
         db.session.add(project)
         db.session.commit()
+        user2project = User2Project(
+            user_id=user.id,
+            project_id=project.id
+        )
+        db.session.add(user2project)
+        db.session.commit()
         for puser in userlist:
             user2project = User2Project(
                 user_id=puser['userID'],
                 project_id=project.id
             )
-            try:
-                db.session.add(user2project)
-                db.session.commit()
-            except Exception as e:
-                return jsonify({
-                }), 500
+            db.session.add(user2project)
+            db.session.commit()
     except Exception as e:
         return jsonify({
         }), 500
     return jsonify({
+        'project_id': str(project.id)
     }), 200
+
+
+@api.route('project/<int:pid>/', methods=['POST', 'DELETE'])
+@login_required
+def project_pid(uid, pid):
+    if request.method == 'POST':
+        intro = request.get_json().get('intro')
+        name = request.get_json().get('name')
+
+        try:
+            project = Project.query.filter_by(id=pid).first()
+            project.name = name
+            project.intro = intro
+
+            db.session.add(project)
+            db.session.commit()
+        except:
+            return jsonify({
+            }), 500
+        return jsonify({
+        }), 200
+    elif request.method == 'DELETE':
+        try:
+            project = Project.query.filter_by(id=pid).first()
+            db.session.delete(project)
+            db.session.commit()
+            user2projects = User2Project.query.filter_by(project_id=pid)
+            for u2p in user2projects:
+                db.session.delete(u2p)
+            db.session.commit()
+            files = File.query.filter_by(project_id=pid)
+            for file in files:
+                db.session.delete(file)
+            db.session.commit()
+        except:
+            return jsonify({
+            }), 500
+        return jsonify({
+        }), 200
+    else:
+        return jsonify({
+        }), 405
